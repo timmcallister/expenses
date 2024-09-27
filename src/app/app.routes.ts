81e29ecf4ae5538e7839109f59657176fc328ba5
Tim McAllister <tim.mcallister84@gmail.com>
2024-09-27 14:25:55 -0500

    Built skeleton

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from  './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ExpenseListComponent } from './expense-list/expense-list.component';
+import { ExpenseFormComponent } from './expense-form/expense-form.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'expenses', component: ExpenseListComponent },
+    { path: 'expense/new', component: ExpenseFormComponent },
+    { path: 'expense/edit/:id', component: ExpenseFormComponent }
+];
