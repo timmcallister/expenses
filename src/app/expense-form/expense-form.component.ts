774ac7ebf665210b2797b54e10ad6b1cc2a8f30a
Tim McAllister <tim.mcallister84@gmail.com>
2024-09-30 20:45:44 -0500

    expense form and list implementation

--- src/app/expense-form/expense-form.component.ts
+++ src/app/expense-form/expense-form.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Expense, ExpenseService } from '../expense.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-expense-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './expense-form.component.html',
   styleUrl: './expense-form.component.css'
 })
-export class ExpenseFormComponent {
+export class ExpenseFormComponent implements OnInit {
+  expense: Expense = {id: 0, description: '', amount: 0, date: ''};
+  isEditing = false;
+  errorMessage = '';
 
+  constructor(
+    private expenseService: ExpenseService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(){
+    const id = this.route.snapshot.params['id'];
+    if (id) {
+      this.isEditing = true;
+      this.expenseService.getExpenses().subscribe((expenses) => {
+        const existingExpense = expenses.find((exp) => exp.id === +id);
+        if (existingExpense) {
+          this.expense = existingExpense;
+        }
+      });
+    }
+  }
+
+  saveExpense() {
+    if (this.isEditing) {
+      this.expenseService.updateExpense(this.expense).subscribe({
+        next: () => this.router.navigate(['./expenses']),
+        error: (error) => {
+          console.error(error);
+          this.errorMessage = 'Could not create the expense.';
+        }
+      })
+    }
+  }
 }
--- src/app/expense-list/expense-list.component.ts
+++ src/app/expense-list/expense-list.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Expense, ExpenseService } from '../expense.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-expense-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './expense-list.component.html',
   styleUrl: './expense-list.component.css'
 })
 export class ExpenseListComponent {
+  expenses: Expense[] = [];
+  errorMessage = '';
 
+  constructor(private expenseService: ExpenseService) {}
+
+  ngOnInit() {
+    this.loadExpenses();
+  }
+
+  loadExpenses() {
+    this.expenseService.getExpenses().subscribe({
+      next: (data) => (this.expenses = data),
+      error: (error) => {
+        console.error(error);
+        this.errorMessage = 'Could not load expenses.';
+      }
+    });
+  }
+
+  deleteExpense(id: number) {
+    if(confirm('Are you sure you want to delete this expense')) {
+      this.expenseService.deleteExpense(id).subscribe({
+        next: () => this.loadExpenses(),
+        error: (error) => {
+          console.error(error);
+          this.errorMessage = 'Could not delete the expense';
+        }
+      })
+    }
+  }
 }
