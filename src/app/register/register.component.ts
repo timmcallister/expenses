0dd71b47532970ecdbd3e91fb33dde11c68602a5
Tim McAllister <tim.mcallister84@gmail.com>
2024-09-30 19:14:47 -0500

    Registration Mockup

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  username = '';
+  password = '';
+  registrationError = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  register() {
+    this.authService.register(this.username, this.password).subscribe({
+      next: (Response) => {
+        if (Response.success) {
+          alert('Registration successful! Please log in.');
+          this.router.navigate(['/login']);
+        }
+      },
+      error: (error) => {
+        console.error(error);
+        this.registrationError = 'Registration failed. Please try again.';
+      }
+    });
+  }
 }
